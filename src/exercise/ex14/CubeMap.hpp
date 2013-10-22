808102144bb98d0abd01083e92aba7d30b0af8e5
Jens Kristoffer Reitan Markussen <jenskristoffer@ubuntu.(none)>
2013-10-22 23:01:01 +0200

    updated with exercises for 22 october group meeting

---
+++ src/exercise/ex14/CubeMap.hpp
@@ -0,0 +1,179 @@
+#ifndef CUBEMAP_H__
+#define CUBEMAP_H__
+
+#include <string>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+#include <stdio.h>
+#include <vector>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "OpenGLError.hpp"
+#include "TGALoader.h"
+
+const GLubyte quad_indices[]= {
+	0, 2, 1, 2, 0, 3, //north face
+	1, 6, 5, 6, 1, 2, //west face
+	5, 7, 4, 7, 5, 6, //south face
+	4, 3, 0, 3, 4, 7, //east face
+	3, 6, 2, 6, 3, 7, //up face
+	0, 5, 4, 5, 0, 1, //down face
+};
+static const GLfloat quad_vertices[] =  {
+	-1.f, -1.f,  1.f,
+	 1.f, -1.f,  1.f,
+	 1.f,  1.f,  1.f,
+	-1.f,  1.f,  1.f,
+
+	-1.f, -1.f, -1.f,
+	 1.f, -1.f, -1.f,
+	 1.f,  1.f, -1.f,
+	-1.f,  1.f, -1.f,
+};
+
+class CubeMap {
+public:
+	CubeMap(std::string base_filename){
+		//Load cubemap from file
+		cubemap = loadCubeMap(base_filename, "tga");
+
+		//Create program
+		std::string vert = "\
+			varying vec3 tex_coord;\
+			uniform mat4 tex_rotate;\
+			void main() {\
+				vec4 pos = gl_ProjectionMatrix*gl_ModelViewMatrix * vec4(gl_Vertex.xyz*2.0, 1.0);\
+				tex_coord = -gl_Vertex.xyz;\
+				gl_Position = pos;\
+			}";
+		
+		std::string frag = "\
+			uniform samplerCube cube_tex;\
+			varying vec3 tex_coord;\
+			void main() {\
+				gl_FragColor = textureCube(cube_tex, tex_coord);\
+			}";
+		
+		const char* vertSource = vert.c_str();
+		
+		GLint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+  		glShaderSource(vertexShaderId, 1, &vertSource, NULL);
+  		glCompileShader(vertexShaderId);
+		  		
+  		const char* fragSource = frag.c_str();
+  		
+  		GLint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+  		glShaderSource(fragmentShaderId, 1, &fragSource, NULL);
+  		glCompileShader(fragmentShaderId);
+		
+		cubemap_program = glCreateProgram();
+    	glAttachShader(cubemap_program, vertexShaderId);
+    	glAttachShader(cubemap_program, fragmentShaderId);
+    	glLinkProgram(cubemap_program);
+		
+		// Create buffer objects
+		glGenBuffers(1, &vertices_buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, vertices_buffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertices), &quad_vertices[0], GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		glGenBuffers(1, &indices_buffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quad_indices), &quad_indices[0], GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+	~CubeMap(){
+		glDeleteBuffers(1, &vertices_buffer);
+		glDeleteBuffers(1, &indices_buffer);
+	}
+
+	void bind(){
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap);
+	}
+	void unbind(){
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+	}
+
+	void render(){
+		glMatrixMode(GL_MODELVIEW);
+		
+		glm::mat4 transform;
+		glGetFloatv(GL_MODELVIEW_MATRIX, glm::value_ptr(transform));
+		
+		transform[3][0] = 0;
+		transform[3][1] = 0;
+		transform[3][2] = 0;
+		
+		glPushMatrix();
+		
+		glLoadMatrixf(glm::value_ptr(transform));
+		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap);
+		
+		glEnableClientState(GL_VERTEX_ARRAY);
+
+		glUseProgram(cubemap_program);
+		glUniform1i(glGetUniformLocation(cubemap_program,"cube_tex"), 0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vertices_buffer);
+		glVertexPointer(3, GL_FLOAT, 0, NULL);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, NULL);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+		glUseProgram(0);
+		
+		glDisableClientState(GL_VERTEX_ARRAY);
+		
+		glPopMatrix();
+	}
+
+private:
+	GLuint loadCubeMap(std::string base_filename, std::string extension) {
+		const char name_exts[6][5] = {"posx", "negx", "posy", "negy", "posz", "negz"};
+		const GLenum faces[6] = {GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+			GL_TEXTURE_CUBE_MAP_POSITIVE_Y, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+			GL_TEXTURE_CUBE_MAP_POSITIVE_Z, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z};
+		GLuint cube_map_name;
+
+		//Allocate texture name and set parameters
+		glGenTextures(1, &cube_map_name);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cube_map_name);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		//Load each face and set texture
+		for (int i=0; i<6; ++i) {
+			std::stringstream filename;
+
+			filename << base_filename << name_exts[i] << "." << extension;
+			tgaInfo* img = tgaLoad(filename.str().c_str()); 
+			if(img->status != 0){
+				std::cout << img->status << std::endl;
+			}
+			
+			glTexImage2D(faces[i], 0, GL_RGB, img->width, img->height, 
+				0, GL_RGB, GL_UNSIGNED_BYTE, (void*)img->imageData);
+			tgaDestroy(img);
+		}
+
+		glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+		return cube_map_name;
+	}
+
+	GLuint cubemap;
+	GLuint vertices_buffer;
+	GLuint indices_buffer;
+	GLint cubemap_program;
+};
+
+#endif
---
+++ src/exercise/ex14/TGALoader.h
@@ -0,0 +1,130 @@
+#define	TGA_ERROR_FILE_OPEN				-5
+#define TGA_ERROR_READING_FILE			-4
+#define TGA_ERROR_INDEXED_COLOR			-3
+#define TGA_ERROR_MEMORY				-2
+#define TGA_ERROR_COMPRESSED_FILE		-1
+#define TGA_OK							 0
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	int status;
+	unsigned char type, pixelDepth;
+	short int width, height;
+	unsigned char *imageData;
+}tgaInfo;
+
+tgaInfo* tgaLoad(const char *filename){
+	FILE *file;
+	tgaInfo *info;
+	int mode,total;
+
+	// allocate memory for the info struct and check!
+	info = (tgaInfo *)malloc(sizeof(tgaInfo));
+	if (info == NULL)
+		return(NULL);
+
+
+	// open the file for reading (binary mode)
+	file = fopen(filename, "rb");
+	if (file == NULL) {
+		info->status = TGA_ERROR_FILE_OPEN;
+		return(info);
+	}
+	
+	// Load header info
+	unsigned char cGarbage;
+	short int iGarbage;
+
+	fread(&cGarbage, sizeof(unsigned char), 1, file);
+	fread(&cGarbage, sizeof(unsigned char), 1, file);
+
+	// type must be 2 or 3
+	fread(&info->type, sizeof(unsigned char), 1, file);
+
+	fread(&iGarbage, sizeof(short int), 1, file);
+	fread(&iGarbage, sizeof(short int), 1, file);
+	fread(&cGarbage, sizeof(unsigned char), 1, file);
+	fread(&iGarbage, sizeof(short int), 1, file);
+	fread(&iGarbage, sizeof(short int), 1, file);
+
+	fread(&info->width, sizeof(short int), 1, file);
+	fread(&info->height, sizeof(short int), 1, file);
+	fread(&info->pixelDepth, sizeof(unsigned char), 1, file);
+
+	fread(&cGarbage, sizeof(unsigned char), 1, file);
+	
+	// check for errors when loading the header
+	if (ferror(file)) {
+		info->status = TGA_ERROR_READING_FILE;
+		fclose(file);
+		return(info);
+	}
+
+	// check if the image is color indexed
+	if (info->type == 1) {
+		info->status = TGA_ERROR_INDEXED_COLOR;
+		fclose(file);
+		return(info);
+	}
+	// check for other types (compressed images)
+	if ((info->type != 2) && (info->type !=3)) {
+		info->status = TGA_ERROR_COMPRESSED_FILE;
+		fclose(file);
+		return(info);
+	}
+
+	// mode equals the number of image components
+	mode = info->pixelDepth / 8;
+	// total is the number of bytes to read
+	total = info->height * info->width * mode;
+	// allocate memory for image pixels
+	info->imageData = (unsigned char *)malloc(sizeof(unsigned char) * 
+															total);
+
+	// check to make sure we have the memory required
+	if (info->imageData == NULL) {
+		info->status = TGA_ERROR_MEMORY;
+		fclose(file);
+		return(info);
+	}
+	
+	//Load img
+	int i;
+	unsigned char aux;
+
+	// mode equal the number of components for each pixel
+	mode = info->pixelDepth / 8;
+	// total is the number of bytes we'll have to read
+	total = info->height * info->width * mode;
+	
+	fread(info->imageData,sizeof(unsigned char),total,file);
+
+	// mode=3 or 4 implies that the image is RGB(A). However TGA
+	// stores it as BGR(A) so we'll have to swap R and B.
+	if (mode >= 3)
+		for (i=0; i < total; i+= mode) {
+			aux = info->imageData[i];
+			info->imageData[i] = info->imageData[i+2];
+			info->imageData[i+2] = aux;
+		}
+	
+	// check for errors when reading the pixels
+	if (ferror(file)) {
+		info->status = TGA_ERROR_READING_FILE;
+		fclose(file);
+		return(info);
+	}
+	fclose(file);
+	info->status = TGA_OK;
+	return(info);
+}
+
+void tgaDestroy(tgaInfo *info){
+	if (info != NULL) {
+		free(info->imageData);
+		free(info);
+	}
+}
